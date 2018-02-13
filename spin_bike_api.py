21c6b7f1839bcbcb3f069fd7e49a5b247e4bf9d0
EgeAydede <ege.aydede@mail.mcgill.ca>
2018-02-13 02:48:47 -0500

    Put request for bike_manager table

--- spin_bike_api.py
+++ spin_bike_api.py
@@ -5,6 +5,7 @@ from flask_restful import Resource, Api
 #Imported local files
 from bike_usage_select import BikeUsageSelectQuery
 from login import LoginQuery
+from account_creation_insert import AccountCreationInsertQuery
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,6 +13,7 @@ api = Api(app)
 #Add route to api with functionality in specified class
 api.add_resource(BikeUsageSelectQuery, '/')
 api.add_resource(LoginQuery, '/auth', endpoint='auth')
+api.add_resource(AccountCreationInsertQuery, '/create')
 
 if __name__ == '__main__':
     app.run(debug=True)
