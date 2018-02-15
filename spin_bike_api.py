985fc39232af1f70e951ab309a0076ce616e95bf
EgeAydede <ege.aydede@mail.mcgill.ca>
2018-02-14 21:06:51 -0500

    Added command to retrieve usage by bike id

--- spin_bike_api.py
+++ spin_bike_api.py
@@ -7,6 +7,7 @@ from flask_cors import CORS
 from bike_usage import BikeUsageAll
 from login import LoginQuery
 from account_creation_insert import AccountCreationInsertQuery
+from usage_by_id import UsageByIDSelect
 
 app = Flask(__name__)
 api = Api(app)
@@ -16,6 +17,7 @@ CORS(app)
 api.add_resource(BikeUsageAll, '/')
 api.add_resource(LoginQuery, '/auth', endpoint='auth')
 api.add_resource(AccountCreationInsertQuery, '/create')
+api.add_resource(UsageByIDSelect, '/usage/<bike_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
