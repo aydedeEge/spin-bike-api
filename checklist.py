9fadbcf3470f9ba662324250d913213422a775c2
abdullahsumbal <abdullahsumbal@gmail.com>
2018-02-22 06:05:00 -0500

    added bike_state

--- checklist.py
+++ checklist.py
@@ -37,14 +37,18 @@ class ChecklistQuery(Resource):
         # clean = 1
         # comments = 1
         # done = 1
+        # comp_state = 1
+        # sb_id = 1
         bm_id = args['bm_id']
         l_id = args['l_id']
         plants_watered =  args['water']
         clean = args['clean']
         comments = args['comments']
         done = args['done']
+        comp_state =  args['comp_state']
+        sb_id = args['sb_id']
 
-        args = request.args
+        # insert into mainteance table
         INSERT_QUERY = "INSERT INTO maintenance VALUES ({m_id},'{date}', {bm_id}, {l_id}, {plants}, {clean}, {comments}, {done})".format(
             m_id=m_id,
             date=maintenance_time,
@@ -54,4 +58,11 @@ class ChecklistQuery(Resource):
             clean=clean,
             comments=comments,
             done=done)
-        result = self.sql.insert_query(INSERT_QUERY)  
+        result = self.sql.insert_query(INSERT_QUERY)
+
+        # insert into bike_state table
+        INSERT_QUERY = "INSERT INTO bike_state ({m_id}, {sb_id}, {comp_state})".format(
+            m_id=m_id,
+            sb_id=sb_id,
+            comp_state=comp_state)
+        result = self.sql.insert_query(INSERT_QUERY)
