2148885bc4d49336b7940161d22129a36b9376f3
floregol <florence.robert-regol@mail.mcgill.ca>
2018-02-23 13:30:48 -0500

    Added Api get for schedule

--- managers.py
+++ managers.py
@@ -13,13 +13,13 @@ class Managers(Resource):
         self.sql = SQLConn()
 
     def get(self):
-        #obtain total usage right away and goes backwards to locations
+        #obtain all managers from database
         ALL_MANAGERS = "SELECT * FROM `bike_manager`"
         all_managers_entries = self.sql.select_query(ALL_MANAGERS)
 
         #group managers by caretakers or admin
         managers_dict = {}
         for role, g in groupby(all_managers_entries, lambda x: x['role']):
-              managers_dict[role] = list(g)
+            managers_dict[role] = list(g)
 
         return managers_dict
