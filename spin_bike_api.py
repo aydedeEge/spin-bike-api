3293c8bab7b9f51487ff7b5b64ad706595f2432b
floregol <florence.robert-regol@mail.mcgill.ca>
2018-02-22 23:47:07 -0500

    Add get request for admins and caretakers

--- spin_bike_api.py
+++ spin_bike_api.py
@@ -8,6 +8,7 @@ from bike_usage import BikeUsageAll
 from login import LoginQuery
 from account_creation_insert import AccountCreationInsertQuery
 from usage_by_id import UsageByIDSelect
+from managers import Managers
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,6 +19,7 @@ api.add_resource(BikeUsageAll, '/')
 api.add_resource(LoginQuery, '/auth', endpoint='auth')
 api.add_resource(AccountCreationInsertQuery, '/create')
 api.add_resource(UsageByIDSelect, '/usage/<bike_id>')
+api.add_resource(Managers, '/managers')
 
 if __name__ == '__main__':
     app.run(debug=True)
