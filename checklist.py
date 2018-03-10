2b0be69106b09c9ea672e2379236ff685d34d668
abdullahsumbal <abdullahsumbal@gmail.com>
2018-03-10 12:10:06 -0500

    can handle multiple bikes

--- checklist.py
+++ checklist.py
@@ -31,25 +31,27 @@ class ChecklistQuery(Resource):
         args = request.args
         m_id = self.getID()
         maintenance_time = datetime.datetime.now()
+
+        # api should look like
+        # 127.0.0.1:5000/checklist?bm_id=1&l_id=1&water=1&clean=1&comments='fdfs'&done=1&sb_ids={}
         # bm_id = 1
         # l_id = 1
-        # plants_watered = 1
-        # clean = 1
-        # comments = 1
+        # plants_watered = True
+        # clean = True
+        # comments = "fsfdfds"
         # done = 1
-        # comp_state = 1
-        # sb_id = 1
+        # sb_ids = {1: True}
+        
         bm_id = args['bm_id']
         l_id = args['l_id']
         plants_watered =  args['water']
         clean = args['clean']
         comments = args['comments']
         done = args['done']
-        comp_state =  args['comp_state']
-        sb_id = args['sb_id']
+        sb_ids = args['sb_ids']
 
         # insert into mainteance table
-        INSERT_QUERY = "INSERT INTO maintenance VALUES ({m_id},'{date}', {bm_id}, {l_id}, {plants}, {clean}, {comments}, {done})".format(
+        INSERT_QUERY = "INSERT INTO maintenance VALUES ({m_id},'{date}', {bm_id}, {l_id}, {plants}, {clean}, '{comments}', {done})".format(
             m_id=m_id,
             date=maintenance_time,
             bm_id=bm_id,
@@ -61,8 +63,12 @@ class ChecklistQuery(Resource):
         result = self.sql.insert_query(INSERT_QUERY)
 
         # insert into bike_state table
-        INSERT_QUERY = "INSERT INTO bike_state VALUES ({m_id}, {sb_id}, {comp_state})".format(
-            m_id=m_id,
-            sb_id=sb_id,
-            comp_state=comp_state)
-        result = self.sql.insert_query(INSERT_QUERY)
+        for sb_id,comp_state in sb_ids.items():
+
+            INSERT_QUERY = "INSERT INTO bike_state VALUES ({m_id}, {sb_id}, {comp_state}, '{maintenance_time}')".format(
+                m_id=m_id,
+                sb_id=sb_id,
+                comp_state=comp_state,
+                maintenance_time=maintenance_time)
+            print(INSERT_QUERY)
+            result = self.sql.insert_query(INSERT_QUERY)
