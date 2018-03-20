ed40e218f316567861d5fe51ab6a7c33d3755786
ModeratelyComfortableChair <jerome.marfleet@mail.mcgill.ca>
2018-03-19 23:22:26 -0400

    Created get request to return user names. \nAdded routing to edit_account in spin_bike_api.py

--- edit_account.py
+++ edit_account.py
@@ -5,16 +5,12 @@ from flask import request
 from flask_restful import Resource
 from SQLConnect import SQLConn
 
-class AccountCreationInsertQuery(Resource):
+class EditAccount(Resource):
 
     def __init__(self):
         self.sql = SQLConn()
 
-    
-
-    def get_usernames(self):
-		GET_PASSWORD="SELECT bm_name FROM `bike_manager`"
-		result = self.sql.select_query(GET_PASSWORD)
-		for name in result:
-			print(name)
-		
+    def get(self):
+        GET_NAMES="SELECT bm_name FROM `bike_manager`"
+        result=self.sql.select_query(GET_NAMES)
+        return result
